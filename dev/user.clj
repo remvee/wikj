47a68513d7046ef7e4376db4507415300124d60a
Remco van 't Veer <remco@remworks.net>
2014-06-28 13:05:31 +0200

    Add dev/user.clj to improve dev repl experience

---
+++ dev/user.clj
@@ -0,0 +1,23 @@
+(ns user
+  (:require [clojure.tools.namespace.repl :refer [refresh]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [wikj.core :as wikj]))
+
+(defonce server-instance (atom nil))
+
+(defn start-server [& {:keys [host port]}]
+  (when @server-instance
+    (throw (Exception. "already started")))
+
+  (when (not (seq @wikj/pages))
+    (wikj/restore-pages))
+
+  (reset! server-instance
+          (run-jetty #'wikj/app {:host (or host "localhost")
+                                 :port (or port 8080)
+                                 :join? false})))
+
+(defn stop-server []
+  (when @server-instance
+    (.stop @server-instance)
+    (reset! server-instance nil)))
--- project.clj
+++ project.clj
@@ -1,14 +1,18 @@
 (defproject wikj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
                  [ring/ring-defaults "0.1.0"]
                  [ring-server "0.3.1"]]
-  
+
   :plugins [[lein-ring "0.8.10"]]
 
   :ring {:init wikj.core/bootstrap!
-         :handler wikj.core/app})
+         :handler wikj.core/app}
+
+  :profiles {:dev
+             {:source-paths ["dev"]
+              :dependencies [[org.clojure/tools.namespace "0.2.3"]]}})
